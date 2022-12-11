56062eb8f6d99393f91ebb924dddae1b1f2575f1
vrecusko <vrecusko20@gmail.com>
2022-12-11 06:40:30 +0100

    bmp pixel loading, simplify logging

--- imgloader.h
+++ imgloader.h
@@ -7,12 +7,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define IMGLOADER_BMP_ID_0   0x00
-#define IMGLOADER_BMP_ID_1   0x01
-#define IMGLOADER_BMP_WIDTH  0x12
-#define IMGLOADER_BMP_HEIGHT 0x16
-#define IMGLOADER_BMP_BITS   0x1C
-#define IMGLOADER_BMP_OFFSET 0x36
+#define IMGLOADER_BMP_OFFSET_ID          0x00
+#define IMGLOADER_BMP_OFFSET_WIDTH       0x12
+#define IMGLOADER_BMP_OFFSET_HEIGHT      0x16
+#define IMGLOADER_BMP_OFFSET_BITS        0x1C
+#define IMGLOADER_BMP_OFFSET_COMPRESSION 0x1E
+#define IMGLOADER_BMP_OFFSET_DATA        0x36
+#define IMGLOADER_BMP_COMPRESSION_NO     0x00
 
 typedef struct bmp
 {
@@ -32,36 +33,38 @@ static void imgloader_free(char** _data)
     *_data = NULL;
 }
 
-bmp_t imgloader_bmp_load(const char* _filepath)
+static void imgloader_puts(const char* _str)
+{
+#ifdef IMGLOADER_LOG
+    puts(_str);
+#endif
+}
+
+static bmp_t imgloader_bmp_load(const char* _filepath)
 {
     bmp_t  b;
     FILE*  f;
     char*  buf;
     long   len;
+    int    size, pad, ofs, x, y;
 
     b.data = NULL;
 
     if ((f = fopen(_filepath, "rb")) == NULL)
     {
-#ifdef IMGLOADER_LOG
-        puts("fopen fail");
-#endif
+        imgloader_puts("fopen fail");
         goto exit;
     }
 
     if (fseek(f, 0, SEEK_END))
     {
-#ifdef IMGLOADER_LOG
-        puts("fseek fail");
-#endif
+        imgloader_puts("fseek fail");
         goto fclose_exit;
     }
 
     if ((len = ftell(f)) < 0)
     {
-#ifdef IMGLOADER_LOG
-        puts("ftell fail");
-#endif
+        imgloader_puts("ftell fail");
         goto fclose_exit;
     }
 
@@ -69,30 +72,46 @@ bmp_t imgloader_bmp_load(const char* _filepath)
 
     if ((buf = (char*) malloc(sizeof(char) * len)) == NULL)
     {
-#ifdef IMGLOADER_LOG
-        puts("malloc fail");
-#endif
+        imgloader_puts("malloc fail");
         goto fclose_exit;
     }
 
     if (fread(buf, len, 1, f) != 1)
     {
-#ifdef IMGLOADER_LOG
-        puts("fread fail");
-#endif
+        imgloader_puts("fread fail");
         goto fclose_exit;
     }
 
-    b.id[0]  = buf[IMGLOADER_BMP_ID_0  ];
-    b.id[1]  = buf[IMGLOADER_BMP_ID_1  ];
+    if (buf[IMGLOADER_BMP_OFFSET_COMPRESSION] != IMGLOADER_BMP_COMPRESSION_NO)
+    {
+        imgloader_puts("unknown bmp compression");
+        goto fclose_exit;
+    }
+
+    b.id[0]  = buf[IMGLOADER_BMP_OFFSET_ID    ];
+    b.id[1]  = buf[IMGLOADER_BMP_OFFSET_ID + 1];
     b.id[2]  = '\0';
-    b.width  = buf[IMGLOADER_BMP_WIDTH ];
-    b.height = buf[IMGLOADER_BMP_HEIGHT];
-    b.bits   = buf[IMGLOADER_BMP_BITS  ];
+    b.width  = buf[IMGLOADER_BMP_OFFSET_WIDTH ];
+    b.height = buf[IMGLOADER_BMP_OFFSET_HEIGHT];
+    b.bits   = buf[IMGLOADER_BMP_OFFSET_BITS  ];
 
-    b.data = (char*) malloc(sizeof(char) * b.width * b.height);
+    size   = b.width * b.height * 3;
+    b.data = (char*) malloc(sizeof(char) * size);
 
-    /* i % 4 == 0 ? i : i + 4 - i % 4 */
+    pad = 4 * (b.width % 4);
+    ofs = IMGLOADER_BMP_OFFSET_DATA;
+
+    for (y = b.height - 1; y >= 0; --y)
+    {
+        for (x = b.width - 1; x >= 0; --x)
+        {
+            b.data[--size] = buf[ofs + x*3    ];
+            b.data[--size] = buf[ofs + x*3 + 1];
+            b.data[--size] = buf[ofs + x*3 + 2];
+        }
+
+        ofs += pad;
+    }
 
     free(buf);
 
@@ -100,11 +119,7 @@ bmp_t imgloader_bmp_load(const char* _filepath)
 
 fclose_exit:
     if (fclose(f))
-    {
-#ifdef IMGLOADER_LOG
-        puts("fclose fail");
-#endif
-    }
+        imgloader_puts("fclose fail");
 
 exit:
     return b;
--- test.c
+++ test.c
@@ -5,6 +5,7 @@
 
 int main(void)
 {
+    int i = 0;
     bmp_t a = imgloader_bmp_load("res/2x3.bmp");
 
     if (a.data == NULL)
@@ -15,6 +16,11 @@ int main(void)
         ".id     = %s\n.width  = %d\n.height = %d\n.bits   = %d\n",
         a.id, a.width, a.height, a.bits
     );
+
+    for (; i < a.width * a.height * 3; ++i)
+    {
+        printf("%02d ", a.data[i]);
+    }
 
     imgloader_free(&a.data);
 
